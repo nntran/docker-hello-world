6e4cbe87a8bccbb88e25017903c97714c81be7aa
Nhi TRAN <nntran@users.noreply.github.com>
2019-09-26 11:13:57 +0200

    Show container ID

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM python:3.7-alpine
 MAINTAINER ntran@ntdt.fr
-ENV FORM_DOCKER_DIR /formation/docker
+ENV APP_DIR /hello-world
 ENV FLASK_APP app.py
 ENV FLASK_RUN_HOST 0.0.0.0
+ENV FLASK_ENV production
 RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-WORKDIR $FORM_DOCKER_DIR
-COPY . $FORM_DOCKER_DIR
+WORKDIR $APP_DIR
+COPY . $APP_DIR
+EXPOSE 5000
 CMD ["flask", "run"]
--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+import socket
 
 # creates a Flask application, named app
 app = Flask(__name__)
+hostname = socket.gethostname()
 
 # a route where we will display a welcome message via an HTML template
 @app.route("/")
-def hello():  
+def hello():
     message = "Hello World!"
-    return render_template('index.html', message=message)
+    return render_template('index.html', message=message, hostname=hostname)
 
 # run the application
-if __name__ == "__main__":  
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.logger.debug('Container ID: %s', hostname)
+    app.run(debug=True, threaded=True)
